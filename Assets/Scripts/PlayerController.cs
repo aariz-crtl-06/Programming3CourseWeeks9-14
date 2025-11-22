ca1aac771c370bcb789b068bc2bbdaf76de47d41
aariz-crtl-06 <aarizza75@gmail.com>
2025-11-22 14:37:26 -0500

    Completed Journal 6.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,12 +6,17 @@ public class PlayerController : MonoBehaviour
     
     Rigidbody2D rb;
 
-    public float maxSpeed = 14f;
-    public float accel = 12f;
-    public float decel = 60f;
+    //Values for the different movement types
+    public float speed = 14f;
+    public float acceleration = 12f;
+    public float deceleration = 60f;
     public float airAccel = 8f;
     public float airDecel = 30f;
 
+    //Ground check to see if player is in contact with any ground objects
+    int groundContacts = 0;
+
+    //Facig direction enum
     public enum FacingDirection
     {
         left, right
@@ -19,41 +25,48 @@ public class PlayerController : MonoBehaviour
     public FacingDirection direction;
     private float moveInput;
 
+    //References the rigidbody component when game starts
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    //Get axis to access movement input
     void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
         UpdateFacingDirection();
     }
 
+    //Runs in fixed update to use physics
     void FixedUpdate()
     {
         MovementUpdate();
     }
 
-  
         private void MovementUpdate()
         {
-        float targetSpeed = moveInput * maxSpeed;
+        //Start moving at the target speed
+        float targetSpeed = moveInput * speed;
+
+        //Difference of target speed by current speed
         float speedDiff = targetSpeed - rb.linearVelocity.x;
 
+        //Acceleration rate to use
         float accelRate = 0f;
 
-        
+
+        //if grounded, use ground accel/decel or else use air accel/decel
         if (IsGrounded())
         {
             if (Mathf.Abs(targetSpeed) > 0.01f)
             {
-                accelRate = accel;
+                accelRate = acceleration;
             }
 
             else
             {
-                accelRate = decel;
+                accelRate = deceleration;
             }
         }
 
@@ -68,13 +81,15 @@ public class PlayerController : MonoBehaviour
                 accelRate = airDecel;
             }
         }
-
+        //Mvement amount
         float movement = speedDiff * accelRate;
 
+        //Apply movement force to rigidbody
         rb.AddForce(Vector2.right * movement);
         }
 
 
+    //If player moves more than zero update facing direction to right
     private void UpdateFacingDirection()
     {
         if (moveInput > 0)
@@ -82,12 +97,14 @@ public class PlayerController : MonoBehaviour
             direction = FacingDirection.right;
         }
 
+        //If player moves less than zero update facing direction to left
         if (moveInput < 0)
         {
             direction = FacingDirection.left;
         }
     }
 
+    //If moving then record movement
     public bool IsWalking()
     {
         if (moveInput != 0)
@@ -101,13 +118,33 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //Uses ground contact to determine if grounded
     public bool IsGrounded()
     {
-        return true;
+        return groundContacts > 0;
     }
 
+    //Get facing direction
     public FacingDirection GetFacingDirection()
     {
         return direction;
+    }
+
+
+    //Checks to see if the stay trigger is activated by the ground, and if so, determines if the player is grounded
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            groundContacts = 1;  // touching ground
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            groundContacts = 0;  // no longer touching ground
+        }
     }
 }
