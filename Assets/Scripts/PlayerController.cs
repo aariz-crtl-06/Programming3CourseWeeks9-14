4e147de08caee7a33bc6e48966175c5b6ddfd8c4
aariz-crtl-06 <aarizza75@gmail.com>
2025-12-07 22:26:11 -0500

    Finish commenting through and implementing the last mechanic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,12 +29,16 @@ public class PlayerController : MonoBehaviour
 
     public GameObject dashEffect;
     public GameObject hoverEffect;
+    public GameObject floatEffect;
 
     bool canDash = true;
     public bool isDashed = false;
 
     public float downStrikeSpeed = 60f;
+    bool canStrike = true;
     public bool isLaunched = false;
+
+    public bool floatyEffect = false;
 
     //Ground check to see if player is in contact with any ground objects
     int groundContacts = 0;
@@ -85,7 +89,21 @@ public class PlayerController : MonoBehaviour
         }
 
         Dash();
-        StartCoroutine(Hover());
+
+        StartCoroutine(StrikeDown());
+
+        //Activate floaty effect when E is pressed
+        if (Input.GetKeyDown(KeyCode.E) && IsGrounded())
+        {
+            floatyEffect = true;
+        }
+
+        //Start floaty coroutine if floaty effect is true
+        if (floatyEffect == true)
+        {
+            floatyEffect = false;
+            StartCoroutine(Floaty());
+        }
     }
 
     //Runs in fixed update to use physics
@@ -112,12 +130,16 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
+    //Horizontal physics mechanic
     void Dash()
     {
+        //If the player presses jump and is in the air and is able to dash, then they will dash in the direction they're moving in, otherwise they go up directly
         if (Input.GetButtonDown("Jump") && !IsGrounded() && canDash == true)
         {
             float x = 0f;
 
+            //Value set to 1 or -1 depending on direction of movement for appropriate dash direction
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
                 x = 1f;
@@ -130,6 +152,7 @@ public class PlayerController : MonoBehaviour
             // vertical dash
             if (x == 0f)
             {
+                //Set the linear velocity higher if going directly up
                 rb.linearVelocity = new Vector2(0f, jumpVelocity * 1.2f);
                 dashEffect.SetActive(true);
                 isDashed = true;
@@ -139,13 +162,14 @@ public class PlayerController : MonoBehaviour
             // horizontal dash
             else
             {
+                //Normalilze dash direction, then set the linear velocity to dash in that direction
                 Vector2 dashDir = new Vector2(x, 0f);
                 dashDir = dashDir.normalized;
                 rb.linearVelocity = dashDir * jumpVelocity * 2f;
                 dashEffect.SetActive(true);
                 isDashed = true;
             }
-
+            //Once dashed, disable to boolean to allow only one dash until grounded again
             canDash = false;
         }
 
@@ -158,28 +182,60 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    IEnumerator Hover()
+
+    //Vertical physics mechanic
+
+    //Enumorator to time the down strike percisely
+    IEnumerator StrikeDown()
     {
-        if(!IsGrounded() && Input.GetKeyDown(KeyCode.S))
+        //If not grounded and the S key is pressed down and can strike is true, then perform the down strike
+        if (!IsGrounded() && Input.GetKeyDown(KeyCode.S) && canStrike)
         {
-            float originalGravity =gravity;
+            canStrike = false;
+            //Store the original gravity value to restore later
+            float originalGravity = gravity;
+            //Temporarily set gravity to a lower value to create a hover effect
             gravity = 0.5f;
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.67f);
+
+            //is launched for animatior, and effect aura is the game object
             isLaunched = true;
             hoverEffect.SetActive(true);
 
-            rb.linearVelocity=Vector2.down * downStrikeSpeed;
+            //Set the velocity down with the strike speed for fast downward movement
+            rb.linearVelocity = Vector2.down * downStrikeSpeed;
+
+            //set the gravity back to original value after the strike
             gravity = originalGravity;
 
-            yield return new WaitForSeconds(0.3f);
+            //After a short delay, reset the states
+            yield return new WaitForSeconds(0.4f);
 
             isLaunched = false;
             isDashed = false;
             hoverEffect.SetActive(false);
-
+            canStrike = true;
         }
-
     }
+
+    //Additonal physics mechanic
+    IEnumerator Floaty()
+    {
+        //Store original gravity value to restore later
+        float originalG = gravity;
+        //Temporarily set gravity to a lower value to create floaty effect
+        gravity = 2f;
+        floatEffect.SetActive(true);
+        yield return new WaitForSeconds(10f);
+
+        //Restore original gravity value after duration
+
+        gravity = originalG;
+        floatEffect.SetActive(false);
+    }
+
+
+
 
         private void MovementUpdate()
         {
